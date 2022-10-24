ccc82e2955ccccd145e4958ffba9a9d63de1345a
Emisael Kisler <ekisler@gmail.com>
2022-10-24 14:39:52 -0400

    Prueba de pull

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/ekisler/kislerportfolio/raw/main/src/Assets/Emisael_Kisler.pdf";
+  "https://raw.githubusercontent.com/ekisler/kislerportfolio/main/src/Assets/Emisael_Kisler.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
