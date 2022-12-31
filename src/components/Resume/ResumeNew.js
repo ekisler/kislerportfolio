352c6d66072edaf1904e5bf7876baa7f1e80d648
Emisael Kisler <ekisler@gmail.com>
2022-12-31 15:28:28 -0400

    reparadas 2 paginas de resumen pdf

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -36,7 +36,10 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <div><Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} /></div>
+          </Document>
+          <Document file={resumeLink} className="d-flex justify-content-center">
+            <div><Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} /></div>
           </Document>
         </Row>
 
